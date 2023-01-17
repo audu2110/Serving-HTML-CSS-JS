cc957fc92708e00d70dd40bfc26ae2a1b2406656
audu2110 <91955066+audu2110@users.noreply.github.com>
2023-01-17 15:16:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,3 +21,35 @@ app.use((req, res, next) => {
 });
 
 app.listen(3000);
+
+
+
+
+
+
+
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const contactRoutes=require('./routes/contact');
+const sucessRoutes=require('./routes/sucess');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(contactRoutes);
+app.use(sucessRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
