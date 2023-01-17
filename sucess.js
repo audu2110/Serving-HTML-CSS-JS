a7aefb9514cdab4f2be2b82f39219610e92b6c97
audu2110 <91955066+audu2110@users.noreply.github.com>
2023-01-17 15:19:02 +0530

    Create sucess.js

---
+++ sucess.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+router.get('/sucess', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'sucess.html'));
+});
+
+module.exports = router;
